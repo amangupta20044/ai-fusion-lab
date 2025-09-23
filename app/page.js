44c887c6d30ce3a1bac990c8d602658da1fec2a7
amangupta20044 <a.mann20044@gmail.com>
2025-09-24 00:54:38 +0530

    added layout and  multimodel ai

--- app/page.js
+++ app/page.js
@@ -2,15 +2,13 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
   const { setTheme } = useTheme();
   return (
     <div>
-      <h2>Welcome to the AI Fusion Lab</h2>
-      <Button>Get Started</Button>
-  <Button onClick={() => { console.log('clicked'); setTheme('dark'); }}>Dark Mode</Button>
-      <Button onClick={() => setTheme('light')}>Light Mode</Button>
+      <ChatInputBox />
     </div>
   );
 }
