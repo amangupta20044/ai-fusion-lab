313fd6e66a794cd91f6d197f39f6173cf8432463
amangupta20044 <a.mann20044@gmail.com>
2025-09-26 19:45:40 +0530

    save user model selected ai free and premium

--- app/_components/AiMultiModels.jsx
+++ app/_components/AiMultiModels.jsx
@@ -1,6 +1,6 @@
 import AiModelList from '@/shared/AiModelList'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 
 import {
   Select,
@@ -12,17 +12,38 @@ import {
 import { Switch } from '@/components/ui/switch'
 import { MessageSquare, Lock } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { SelectGroup, SelectLabel } from '@radix-ui/react-select'
+import { AiSelectedModelContext } from '@/context/AiSelectedModelContext'
+import { useUser } from '@clerk/nextjs'
 
 function AiMultiModels() {
+    const {user}=useUser();
+
     const [aiModelList,setAiModelList]=useState(AiModelList)
+
+    const {aiSelecteModels,setAiSelectedModels}=useContext(AiSelectedModelContext);
 
     const onToggleChange = (model, value) => {
       setAiModelList(prev =>
         prev.map(m =>
-          m.model === model ? { ...m, enable: value } : m
-        )
-      );
+          m.model === model ? { ...m, enable: value } : m))
+        
     };
+
+    const onSelectValue=async(parentModel,value)=>{
+      setAiSelectedModels(prev=>({
+        ...prev,
+        [parentModel]:{
+         modelId:value
+      }
+      }))
+      // Update to firebase database
+      const docRef=doc(db,"users", user?.primaryEmailAddress?.emailAddress);
+      await updateDoc(docRef,{
+        aiSelectedModelPref: aiSelecteModels
+      })
+    };
+
   return (
     <div className='flex flex-1 h-[65vh] border-b'>
         {aiModelList.map((module, index) => (
@@ -36,13 +57,27 @@ function AiMultiModels() {
                   width={24} height={24}
                 />
                 {module.enable && <Select>
+                  <Select defaultValue={aiSelecteModels?.[module.model]?.modelId} 
+                  onValueChange={(value)=>onSelectValue(module.model,value)}
+                  disabled={module.premium}></Select>
                   <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder={module.subModel[0].name} />
+                    <SelectValue placeholder={aiSelecteModels[module.model].modelId} />
                   </SelectTrigger>
                   <SelectContent>
-                    {module.subModel.map((subModel, index) => (
-                      <SelectItem key={index} value={subModel.name}>{subModel.name}</SelectItem>
-                    ))}
+                    <SelectGroup className='px-3'>
+                      <SelectLabel className='text-sm text-gray-400'>Free</SelectLabel>
+                      {module.subModel.map((subModel, index) => subModel.premium == false && (
+                        <SelectItem key={index} value={subModel.name}>{subModel.id}</SelectItem>
+                      ))}
+                    </SelectGroup>
+                     <SelectGroup className='px-3'>
+                      <SelectLabel className='text-sm text-gray-400'>Premium</SelectLabel>
+                      {module.subModel.map((subModel, index) => subModel.premium == true && (
+                        <SelectItem key={index} value={subModel.name} disabled={subModel.premium}>
+                          {subModel.name} {subModel.premium && <Lock className='h-4 w-4' />}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
                   </SelectContent>
                 </Select>}
               </div>
